e5890fec95204ca6721fea88fcc82a764ff910fd
AmanBamrel <amanmagar707@gmail.com>
2026-05-29 11:37:00 +0200

    'oefC deel1 klaar'

---
+++ les2_oefC.py
@@ -0,0 +1,12 @@
+getal1 = int(input('voer een getal in '))
+getal2 = int(input(' voer een getal in '))
+
+som = getal1 + getal2
+verschil = getal1 - getal2 
+product = getal1 * getal2
+deling = getal1 / getal2
+
+print (f' som : {som}')
+print (f' verschil:{verschil}')
+print (f' product:{product}')
+print (f'deling : {deling}')
